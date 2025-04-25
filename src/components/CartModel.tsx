449f70c5fb680ec9715596e82cf6fb7fbebcf43f
toony10 <amiranton257@gmail.com>
2025-04-25 03:11:25 +0300

    cart model

---
+++ src/components/CartModel.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { useRouter } from 'next/navigation'
+import {
+    DropdownMenuContent,
+    DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu"
+
+export default function CartModel() {
+    const router = useRouter()
+
+    return (
+        <DropdownMenuContent className='p-0  m-5 w-72'>
+            <div className='p-2 text-xl font-bold text-center'>Shopping Cart</div>
+
+
+            {/* ITEM */ }
+            <div className='flex w-full items-center gap-5 p-2 '>
+                <div className='w-1/3'>
+                    <Image src='/assets/product.png' alt='cart' width={ 80 } height={ 80 } className='rounded-sm' />
+                </div>
+                <div className='flex flex-col justify-between gap-2 p-2 h-full w-full'>
+                    <div className='flex items-center justify-between'>
+                        <div className='text-md'>Name</div>
+                        <div className='text-sm font-bold'>Price</div>
+                    </div>
+                    <div className='flex items-center justify-between'>
+                        <div className='text-md'>Qty: 1</div>
+                        <div className='text-red-700 text-sm w-12 cursor-pointer'>Remove</div>
+                    </div>
+                </div>
+            </div>
+
+            <DropdownMenuSeparator className='m-1' />
+            <div className='flex items-center justify-between p-2'>
+                <div className='text-md'>Total</div>
+                <div className='text-sm text-center font-bold'>$ 100.00</div>
+            </div>
+            <p className='text-center text-sm px-1 text-gray-500 font-semibold'>Shipping and taxes calculated at checkout</p>
+            <div className='flex items-center justify-between p-2'>
+                <Button className='bg-white text-black border cursor-pointer' onClick={ () => router.push('/cart') }>View Cart</Button>
+                <Button className='bg-primary cursor-pointer' onClick={ () => router.push('/checkOut') }>Check out</Button>
+            </div>
+        </DropdownMenuContent>
+    )
+}
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -10,17 +10,16 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from './ui/button'
-// import { useState } from 'react'
+import CartModel from './CartModel'
 
 export default function NavIcons() {
-    const isLogin = false;
+    const isLogin = true;
     const router = useRouter()
 
 
     return (
         <div className='flex items-center justify-between gap-2 md:gap-4'>
             { isLogin ?
-
                 <>
                     <DropdownMenu>
                         <DropdownMenuTrigger className='cursor-pointer'>
@@ -30,10 +29,23 @@ export default function NavIcons() {
                             <DropdownMenuLabel className='cursor-pointer hover:bg-gray-400 p-2'>
                                 <Link href='/'>Profile</Link>
                             </DropdownMenuLabel>
-                            <DropdownMenuSeparator className='m-0' />
+                            <DropdownMenuSeparator className='mx-1' />
                             <DropdownMenuLabel className='cursor-pointer hover:bg-gray-400'>Logout</DropdownMenuLabel>
                         </DropdownMenuContent>
-                    </DropdownMenu><Image src='/assets/notification.png' alt='' width={ 20 } height={ 20 } className='cursor-pointer' /><Image src='/assets/cart.png' alt='' width={ 20 } height={ 20 } className='cursor-pointer' /></>
+                    </DropdownMenu>
+
+                    <Image src='/assets/notification.png' alt='' width={ 20 } height={ 20 } className='cursor-pointer' />
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className='cursor-pointer'>
+                            <div className='relative'>
+                                <div className='absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-4 h-4 text-center text-xs'>1</div>
+                                <Image src='/assets/cart.png' alt='cart' width={ 20 } height={ 20 } className='cursor-pointer' />
+                            </div>
+                        </DropdownMenuTrigger>
+                        <CartModel />
+                    </DropdownMenu>
+                </>
                 : <Button className='bg-primary cursor-pointer' onClick={ () => router.push('/login') }>Login</Button> }
 
         </div>
