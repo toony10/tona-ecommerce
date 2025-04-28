b4e07fd4e66ea49a03919efdaa013f0effbb87de
toony10 <amiranton257@gmail.com>
2025-04-29 02:40:38 +0300

    Featured Products2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import FeaturedProducts from "@/components/FeaturedProducts";
-import ProductList from "@/components/ProductList";
+import FeaturedProducts from "@/components/ProductsList";
+import ProductsList from "@/components/ProductsList";
 import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
     <div>
       <Slider />
-      <ProductList />
-      <FeaturedProducts />
+
+      <div className="px-10 md:px-24 m-10">
+        <h1 className='text-3xl font-semibold text-gray-950 mb-12 text-center md:text-start'>Featured Products</h1>
+        <ProductsList />
+      </div>
     </div>
   );
 }
