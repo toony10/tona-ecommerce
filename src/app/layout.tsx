27882eb0f32fc52bad8d6002f330cfde70c2e4b0
toony10 <amiranton257@gmail.com>
2025-06-03 16:55:36 +0300

    Auth2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
