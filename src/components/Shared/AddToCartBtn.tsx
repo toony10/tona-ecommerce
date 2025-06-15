532bff8fc4e253bead8d2adefbefbb7346e2708a
toony10 <amiranton257@gmail.com>
2025-06-15 03:10:55 +0300

    set sizes

--- src/components/Shared/AddToCartBtn.tsx
+++ src/components/Shared/AddToCartBtn.tsx
@@ -8,9 +8,9 @@ type AddToCartBtnProps = {
 };
 
 const AddToCartBtn: React.FC<AddToCartBtnProps> = ({
-    onClick,
-    disabled = false,
-    label = 'Add to Cart',
+    // onClick,
+    // disabled = false,
+    // label = 'Add to Cart',
 }) => (
     <Button className="w-24 px-12 rounded-full border-[1px] font-semibold border-primary text-primary cursor-pointer hover:bg-primary hover:text-white transition-all duration-300">Add To Cart</Button>
 );
