1a550a4ec36c8f96b4f0227c950f514b44b9276c
toony10 <amiranton257@gmail.com>
2025-07-26 20:55:30 +0300

    Edit navbar (mobile version)

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -4,6 +4,11 @@ import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
+import { Button } from "./ui/button"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import Logo from "./Shared/Logo"
 
 interface MenuProps {
     Links: { name: string; href: string }[];
@@ -16,24 +21,54 @@ export default function MobileMenu({ Links }: MenuProps) {
     }
 
     return (
-        <div>
-            <Image unoptimized src='/assets/menu.png' width={ 28 } height={ 28 } alt="#" className="cursor-pointer md:hidden" onClick={ () => toggleMenu() } />
-            <ul className="fixed z-50 bg-gray-900 text-white text-2xl font-semibold tracking-widest left-0 top-32 w-full h-[100vh] flex flex-col justify-center items-center gap-8 container" style={ { display: isOpen ? 'flex' : 'none' } }>
-                <span className="text-4xl font-bold  absolute top-5 right-5" onClick={ () => toggleMenu() }>X</span>
-                { isOpen && (
-                    Links.map((link, index) => (
-                        <li key={ index } onClick={ () => {
-                            router.push(link.href);
-                            toggleMenu();
-                        } }>
-                            <Link href={ link.href }>
-                                { link.name }
-                            </Link>
-                        </li>
-                    ))
-                )
-                }
-            </ul>
-        </div>
+        // <div>
+        //    
+        //     <ul className="fixed z-50 bg-gray-900 text-white text-2xl font-semibold tracking-widest left-0 top-32 w-full h-[100vh] flex flex-col justify-center items-center gap-8 container" style={ { display: isOpen ? 'flex' : 'none' } }>
+        //         <span className="text-4xl font-bold  absolute top-5 right-5" onClick={ () => toggleMenu() }>X</span>
+        //         { isOpen && (
+        //             Links.map((link, index) => (
+        //                 <li key={ index } onClick={ () => {
+        //                     router.push(link.href);
+        //                     toggleMenu();
+        //                 } }>
+        //                     <Link href={ link.href }>
+        //                         { link.name }
+        //                     </Link>
+        //                 </li>
+        //             ))
+        //         )
+        //         }
+        //     </ul>
+        // </div>
+
+        <Dialog>
+            <form>
+                <DialogTrigger asChild>
+                    <Image unoptimized src='/assets/menu.png' width={ 28 } height={ 28 } alt="#" className="cursor-pointer md:hidden" />
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle className="flex justify-center items-center">
+                            <Logo />
+                        </DialogTitle>
+                    </DialogHeader>
+                    <ul className="flex flex-col items-center gap-10 text-xl font-bold text-gray-900 my-10">
+                        {
+                            Links.map((link, index) => (
+                                <DialogClose key={ index } asChild>
+                                    <li onClick={ () => {
+                                        router.push(link.href);
+                                    } }>
+                                        { link.name }
+                                    </li>
+                                </DialogClose>
+                            )
+                            )
+                        }
+                    </ul>
+
+                </DialogContent>
+            </form>
+        </Dialog>
     )
 }
