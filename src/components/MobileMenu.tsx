38fae7fc6269ce146570168354310b7f75018b66
toony10 <amiranton257@gmail.com>
2025-07-26 21:23:09 +0300

    Edit navbar (mobile version)

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
