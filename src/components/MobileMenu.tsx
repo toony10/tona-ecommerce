8beac410b60d69a0e16764f1a89b72159facb4b0
toony10 <amiranton257@gmail.com>
2025-07-26 21:17:17 +0300

    Edit navbar (mobile version)

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,13 +1,6 @@
-"use client"
-
 import Image from "next/image"
-import Link from "next/link"
 import { useRouter } from "next/navigation"
-import { useState } from "react"
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
-import { Button } from "./ui/button"
-import { Label } from "./ui/label"
-import { Input } from "./ui/input"
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
 import Logo from "./Shared/Logo"
 
 interface MenuProps {
@@ -15,32 +8,7 @@ interface MenuProps {
 }
 export default function MobileMenu({ Links }: MenuProps) {
     const router = useRouter();
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleMenu = () => {
-        setIsOpen(!isOpen)
-    }
-
     return (
-        // <div>
-        //    
-        //     <ul className="fixed z-50 bg-gray-900 text-white text-2xl font-semibold tracking-widest left-0 top-32 w-full h-[100vh] flex flex-col justify-center items-center gap-8 container" style={ { display: isOpen ? 'flex' : 'none' } }>
-        //         <span className="text-4xl font-bold  absolute top-5 right-5" onClick={ () => toggleMenu() }>X</span>
-        //         { isOpen && (
-        //             Links.map((link, index) => (
-        //                 <li key={ index } onClick={ () => {
-        //                     router.push(link.href);
-        //                     toggleMenu();
-        //                 } }>
-        //                     <Link href={ link.href }>
-        //                         { link.name }
-        //                     </Link>
-        //                 </li>
-        //             ))
-        //         )
-        //         }
-        //     </ul>
-        // </div>
-
         <Dialog>
             <form>
                 <DialogTrigger asChild>
